0668ffaa7de2698c6706eff55a68a8b08c63721a
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-02-06 18:08:37 -0600

    remove futures import

--- s3helpers.py
+++ s3helpers.py
@@ -1,13 +1,6 @@
-
 import sys
 import os
 import re
-from builtins import *
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
 
 
 class S3HelperException(Exception):
