70b5751b8bab63d452efb3439bd7053c1aa1f3e6
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-03-05 15:39:54 -0600

    Better logging for routemsg.py

--- routemsg.py
+++ routemsg.py
@@ -1,3 +1,4 @@
+import json
 import re
 from pprint import pprint
 
@@ -5,16 +6,23 @@ from pprint import pprint
 def routemsg(r, agave_dest):
     # Kick off downstream Reactors by filename glob match
     message = {'uri': agave_dest}
+    log_msg = json.dumps(message, sort_keys=True, separators=(',', ':'))
     for routename, globs in r.settings.routings.items():
         actor_id = r.settings.linked_reactors.get(routename, {}).get('id')
         for glob in globs:
             if re.compile(glob).search(agave_dest):
+                r.logger.debug('Route: dest={}, content={}'.format(actor_id, log_msg))
                 try:
                     if r.local is False:
-                        r.send_message(actor_id, message=message)
+                        resp = r.send_message(actor_id, message=message)
+                        if resp is not None:
+                            if 'executionId' in resp:
+                                r.logger.debug(
+                                    'Route: executionId={}, actorId={}'.format(
+                                        resp['executionId'], actor_id))
                     else:
                         pprint(message)
                     break
                 except Exception as exc:
-                    r.on_failure('Failed to launch {}:{} for {}'.format(
+                    r.on_failure('Route: Failed to launch {}:{} for {}'.format(
                         routename, actor_id, agave_dest), exc)
